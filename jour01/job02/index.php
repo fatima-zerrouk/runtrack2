a5995458b3e3220a8042715567d542b9be1a6580
fatima-zerrouk <fatima-zerrouk@laplateforme.io>
2025-07-09 10:40:37 +0200

    Ajout Job02 PHP

---
+++ jour01/job02/index.php
@@ -0,0 +1,12 @@
+<?php
+// Voici un commentaire en ligne
+ $str ="Hello La Plateforme";
+echo $str ."<br>";
+/*
+Voici un commentaore 
+en un bloc
+*/
+ $str ="Hello La Plateforme";
+echo $str;
+
+?>
