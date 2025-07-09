3de237ef7a19533e2978e2f39887c19d6a4bf1de
fatima-zerrouk <fatima-zerrouk@laplateforme.io>
2025-07-09 10:17:03 +0200

    Ajout Job01 PHP

---
+++ jour01/job01/index.php
@@ -0,0 +1,25 @@
+ <?php
+
+ $str ="La Plateforme";
+echo $str;
+
+$str2 = "Vive";
+$str3 = "!";
+echo "<br>" . $str2 . " " . $str . " " . $str3;
+
+$val = 6;
+echo "<br>" . $val;
+
+$val += 4;
+echo "<br>" . $val;
+
+$myBool = true;
+echo "<br>" ;
+var_dump($myBool);
+
+$myBool = false;
+echo "<br>";
+var_dump($myBool);
+
+
+ ?>
