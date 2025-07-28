4060f35eb252259d6c35e0c7d24dd981a39ec8e8
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2025-07-28 16:43:17 +0200

    Exercice jour 3 job 2 terminer

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -1,6 +1,12 @@
 <?php
 $str = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
-for  {
+
+//boucle for pour parcourir la chaîne
+//commence à 0 le premier caractère
+// strlen($str) récupère la longueur de la chaîne de caractères en comptant 
+//tant que $i est plus petit que $str augmente $i de 2 à chaque tour pour sauter un caractère
+for ($i = 0; $i < strlen($str); $i += 2) {
     
+    echo $str[$i];
 }
 ?>
