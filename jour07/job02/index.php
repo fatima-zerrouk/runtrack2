61f63a97296d85699dd9008ff830adfe73106700
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2025-08-06 14:22:44 +0200

    Ajout de commentaire

--- jour07/job02/index.php
+++ jour07/job02/index.php
@@ -2,15 +2,17 @@
 // $jour c'est une variable qui est vide pour que ça soit false parce que si c'es vide c'est 0 donc false
 // pour que la variable soit true il faut mettre une valeur et donc c'est 1 
 $jour = "";
+
 // fonction personnalisée qui s'appelle "bonjour()" 
-// avec l'argument $jour entre parenthèses 
+// avec le paramètre $jour entre parenthèses 
 function bonjour($jour){
+
 // si  $jour vaut true la fonction doit afficher Bonjour
     if ($jour == true){
         echo "Bonjour";
 
 // si  $jour vaut false la fonction doit afficher Bonsoir
-   } if ($jour == false) {
+   } elseif ($jour == false) {
         echo "Bonsoir";
     }
 }
