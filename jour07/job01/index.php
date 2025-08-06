b05601619fe10a21de25919920b1295ff6867348
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2025-08-06 10:24:13 +0200

    Execice jours 7 job 1 terminer

---
+++ jour07/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+// Fonction personnalisé qui s'appelle "hello()"
+function hello(){
+// La fonction a pour instruction d'afficher "Hello LaPlateforme!"
+    echo "Hello LaPlateforme!";
+}
+/* Une fonction ne s’exécute pas toute seule.
+ Il va falloir l’appeler dans le code pour déclencher 
+ son exécution.
+ Pour que la fonction s'affiche, 
+ il suffit de mettre son nom avec des parenthèses.
+ Il est possible d'appeler une fonction autant de 
+ fois que je veux pour gagner du temps
+ */
+hello();
+
+?>
