3832a477da9664be29c77d4c4f3a27281d75cea8
fatima-zerrouk <fatima.zerrouk@laplateforme.io>
2025-08-07 10:51:07 +0200

    Ajout de commentaires

--- jour07/job05/index.php
+++ jour07/job05/index.php
@@ -1,9 +1,11 @@
 <?php
-
+// fonction personnalisée qui s'appelle "occurrences()" 
+// avec le paramètre $str, $char
 function occurrences($str, $char){
 /* substr_count veut dire compte combien 
   de "o" y il y a dans $str*/ 
    return substr_count($str, $char);
 }
+// echo de la fonction qui va afficher le nombre de o donc 2
 echo occurrences("Bonjour", "o");
 ?>
